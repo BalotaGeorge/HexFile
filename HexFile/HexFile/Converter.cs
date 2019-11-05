7a1459ca34b96bfceb1f745ac2c665c91053a273
BalotaGeorge <56297936+BalotaGeorge@users.noreply.github.com>
2019-11-05 16:36:23 +0300

    Add files via upload

---
+++ HexFile/HexFile/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexFile
+{
+    class Converter
+    {
+        public static string ConvertBinaryToHex(string InputNumber)
+        {
+            int DecimalValue = 0;
+            for (int i = 0; i < InputNumber.Length; i++)
+                DecimalValue += (InputNumber[i] - '0') * (int)Math.Pow(2, InputNumber.Length - i - 1);
+            string ret = "";
+            char[] sym = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            int aux = DecimalValue, k = 0;
+            while (aux != 0) { k++; aux /= 16; }
+            int[] digits = new int[k];
+            int index = 0;
+            while (DecimalValue != 0)
+            {
+                digits[index++] = DecimalValue % 16;
+                DecimalValue /= 16;
+            }
+            for (int i = index - 1; i >= 0; i--) ret += sym[digits[i]];
+            return ret;
+        }
+        public static string ConvertTextToBinay(string TextSample)
+        {
+            string ret = "";
+            for (int i = 0; i < TextSample.Length; i++)
+            {
+                ret += ConvertDecimalToBinary(TextSample[i]);
+            }
+            return ret;
+        }
+        public static string ConvertDecimalToBinary(int DecimalValue)
+        {
+            string ret = "";
+            char[] sym = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            int aux = DecimalValue, k = 0;
+            while (aux != 0) { k++; aux /= 2; }
+            int[] digits = new int[k];
+            int index = 0;
+            while (DecimalValue != 0)
+            {
+                digits[index++] = DecimalValue % 2;
+                DecimalValue /= 2;
+            }
+            for (int i = index - 1; i >= 0; i--) ret += sym[digits[i]];
+            return ret;
+        }
+    }
+}
---
+++ HexFile/HexFile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexFile
+{
+    class Program
+    {
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            string input = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                switch(r.Next(3))
+                {
+                    case 0:
+                        input += (char)r.Next(65, 91);
+                        break;
+                    case 1:
+                        input += (char)r.Next(97, 123);
+                        break;
+                    case 2:
+                        input += (char)r.Next(48, 58);
+                        break;
+                }
+                if (i % 100 == 0) input += "\n";
+            }
+            File.WriteAllText("InputFile.txt", input);
+            string data = File.ReadAllText("InputFile.txt");
+            string binary = Converter.ConvertTextToBinay(data);
+            string outputhex = "";
+            int lineCounter = 0;
+            for (int i = 8; i < binary.Length; i += 8) 
+            {
+                string Byte = binary.Substring(i - 8, 8);
+                outputhex += Converter.ConvertBinaryToHex(Byte);
+                outputhex += " ";
+                lineCounter++;
+                if (lineCounter >= 16) 
+                {
+                    lineCounter = 0;
+                    outputhex += "\n";
+                }
+            }
+            File.WriteAllText("OutputFile.txt", outputhex);
+
+            Console.ReadLine();
+        }
+    }
+}
